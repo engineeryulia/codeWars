8290c0352e47fe74cd5ec4be3eb979a9cb7256d3
Yulia Lopatko <engineeryulia@gmail.com>
2022-02-12 19:49:10 -0700

    TypeError: convertLowerCase is not a function

---
+++ kata_8/lowerCase.js
@@ -0,0 +1,6 @@
+//Beginner friendly: Lowercase letters
+//https://www.codewars.com/kata/58aacf3e048560c705000799/train/javascript
+let convertLowerCase = "Hello world!" 
+let result = convertLowerCase.toLowerCase()
+console.log(result)
+
