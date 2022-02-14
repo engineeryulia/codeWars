0c601fb7f78735cd39ee85dc4c8beb9c5e068692
Yulia Lopatko <engineeryulia@gmail.com>
2022-02-14 12:41:37 -0700

    SumTheString

--- kata_8/RemoveFirstAndLastCaracters.js
+++ kata_8/RemoveFirstAndLastCaracters.js
@@ -1,4 +1,4 @@
 //https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/javascript
 
-const removeChar = str => str = str.slice(1, -1)
-console.log(removeChar('string'))
+const removeChar = (str) => (str = str.slice(1, -1))
+console.log(removeChar("string"))
--- kata_8/evenOrOdd.js
+++ kata_8/evenOrOdd.js
@@ -1,14 +1,12 @@
 //Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 //function even_or_odd(number) {}
 
-const even_or_odd = n => {
-if (n % 2 === 0) {
+const even_or_odd = (n) => {
+  if (n % 2 === 0) {
     return "Even"
-    
-} else {
+  } else {
     return "Odd"
-    
-}
+  }
 }
 
 console.log(even_or_odd(5))
